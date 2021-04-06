17194c3a9da2247ecfb4bea1e03eb2005398f457
Tannen <ch.hillebrand@googlemail.com>
2021-04-06 10:58:47 +0200

    HelloControllerTest passed

---
+++ src/test/java/com/example/demo/HelloControllerTests.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTests {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void sayHello_noParam_rtnHelloWorld() throws Exception {
+// /hello string "Hello World"
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello World"));
+    }
+}
